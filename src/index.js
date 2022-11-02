69f9ba0b93f3a0fa16bbb3c1e3c4b54becbc4ee7
Marcos Cavalcante <54518635+MarcosRaoi@users.noreply.github.com>
2022-11-02 14:03:45 -0300

    Added one of the best polished games: Zen TicTacToe
    Bom, adicionado finalmente o Zen Tic-Tac-Toe ao portifólio, ou Jogo da Veia, Veia do Tic Toc, Ana's TicTocTae, já teve vários nomes, AHSUHSUAH, enfim, fechamos no Zen Tic-Tac-Toe, mais comercial. Ele está num nível bem bom "de mercado", daria pra colocar numa playstore da vida, apesar de ser simples, já que é um jogo da velha clássico. Talvez colocar algo pra aumentar a rejogabilidade, talvez implementar uma IA pra jogar contra a pessoa, enfim, tem várias features que poderiam ser adicionadas, mas por enquanto é issae. Resolvi uns bugs dele, dei umas melhorasdinhas, e tou subindo :D

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Footer from "./react/Footer.js";
 import MenuNavigation from "./react/MenuNavigation.js";
 
-const PORTFOLIO_VERSION = "1.15.0";
+const PORTFOLIO_VERSION = "1.16.0";
 const ID_MENU_NAVIGATION = "topper";
 const ID_FOOTER = "bottom";
 
