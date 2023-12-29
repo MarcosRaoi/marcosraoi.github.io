6c51d31d033a591849c2ea0c02506813e01f6961
Marcos Cavalcante <54518635+MarcosRaoi@users.noreply.github.com>
2023-12-29 12:07:17 -0300

    ADICIONA O GODOT GAME CELLS EDITOR! :D
    aEEEEEE, Aeeee!!! Programinha que eu fiz em Godot para me ajudar a editar as GameCells <3

--- src/getLocalJson.js
+++ src/getLocalJson.js
@@ -1,3 +1,7 @@
+import localJsonGameData from "../json/game_cells.json" assert { type: "json" };
+//import("../json/game_cells.json", {assert: {type: "json"} });
+
 export function localGameData() {
-    return false;
+    console.log("local json game data", localJsonGameData);
+    return localJsonGameData;
 }
